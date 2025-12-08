10d830ea70c89b1a340e563b69cd4c00ab54292c
hohserg1 <hohserg1@yandex.ru>
2025-12-08 15:53:12 +0300

    support concurrent model making

--- src/main/scala/hohserg/cursed/snow/Memoized.scala
+++ src/main/scala/hohserg/cursed/snow/Memoized.scala
@@ -1,11 +1,12 @@
 package hohserg.cursed.snow
 
-import java.util.function
+import java.util.function.{Function=>JFunction}
+import java.util.concurrent.{ConcurrentHashMap=>JConcurrentHashMap}
 
 object Memoized {
   def apply[A, B](f: A => B): A => B = {
-    val cache = new java.util.HashMap[A, B]()
-    val javaFunctionWrapper = new function.Function[A, B] {
+    val cache = new JConcurrentHashMap[A, B]()
+    val javaFunctionWrapper = new JFunction[A, B] {
       override def apply(t: A): B = f(t)
     }
     cache.computeIfAbsent(_, javaFunctionWrapper)
